5d3dc7d0a48c787c2b3d736d69d1997f20af99de
Chris Bandrowsky <chris.bandrowsky@gmail.com>
2017-07-27 14:55:08 -0600

    initial component structure and styling

--- src/App.js
+++ src/App.js
@@ -2,17 +2,46 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import ItemHolder from './Components/ItemHolder/ItemHolder'
+// import CartItemHolder from './Components/CartItemHolder'
+// import RecentOrderHolder from './Components/RecentOrderHolder'
+
+
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      items: [],
+      orders: []
+    }
+  }
+  componentDidMount(){
+    fetch('/api/v1/items')
+      .then(data => data.json())
+      .then((items) => {
+        this.setState({
+          items: items
+        })
+      })
+
+    fetch('/api/v1/orders')
+      .then(data => data.json())
+      .then((orders) => {
+        this.setState({
+          orders: orders
+        })
+      })
+  }
+
+  addToCart(item){
+    console.log(item)
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Amazonbayetsy</h1>
+        <ItemHolder items = {this.state.items} addToCart = {this.addToCart.bind(this)}/>
       </div>
     );
   }
